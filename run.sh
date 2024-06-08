2c7396dfc0bf1c12e092090e453ba6d88bfc2130
Shad0wMazt3r <er123pratyaksha@gmail.com>
2024-06-08 19:19:23 +0530

    minor changes

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 python3 web_honeypot.py &
-honeypot_id=$!
+web_honeypot_id=$!
 
-echo "Honeypot PID: $honeypot_id"
+echo "Web Honeypot PID: $web_honeypot_id"
+
+python3 ssh_honeypot.py &
+ssh_honeypot_id=$!
+
+echo "SSH Honeypot PID: $ssh_honeypot_id"
--- start.sh
+++ start.sh
@@ -7,7 +7,9 @@ if [ ! -f "$FILE" ]; then
     sudo apt-get install python3 python3-venv python3-pip php php-cgi php-mysql php-xml
     # python3 -m venv honeypot-env
     sudo pip3 install -r requirements.txt
+    chmod +x run.sh
     echo "Installation complete" > .env_var
 else
     echo "Already installed"
+    bash run.sh
 fi
