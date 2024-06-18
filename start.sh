47a57cbf0245e95a2cff643c0de25caea3fd4445
Shad0wMazt3r <er123pratyaksha@gmail.com>
2024-06-19 03:26:08 +0530

    minor edit

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 FILE=".env_var"
 if [ ! -f "$FILE" ]; then
     sudo apt update
-    sudo apt install python3
-    sudo apt-get install python3 python3-venv python3-pip php php-cgi php-mysql php-xml
+    sudo apt install python3 python-is-python3 python3-venv python3-pip php php-cgi php-mysql php-xml
     # python3 -m venv honeypot-env
     sudo pip3 install -r requirements.txt
     chmod +x run.sh
