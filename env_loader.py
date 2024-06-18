04021fd9e25555e78fbaba3b844aa7a0b749fd6b
Shad0wMazt3r <er123pratyaksha@gmail.com>
2024-06-19 03:24:47 +0530

    added banner loading and save functions

--- env_loader.py
+++ env_loader.py
@@ -4,3 +4,16 @@ def load(env_var):
             if line.startswith(env_var):
                 return line.split("=")[1].strip()
     return None
+def save(env_var, val):
+    with open(".config_honeyeasy", "a") as f:
+        f.write(f"{env_var}={val}\n")
+
+def banner_load(env_var):
+    env_banner = env_var + "_BANNER"
+    banner = ""
+    with open(".config_honeyeasy", "r") as f:
+        for line in f:
+            if line.startswith(env_banner):
+                banner = line.split("=")[1].strip().replace("\\n", "\n").replace('"', "")
+                return banner
+                
