eb2df9bee544704f97c4ea23db2d7245ae3a4338
Shad0wMazt3r <er123pratyaksha@gmail.com>
2024-06-20 21:02:56 +0530

    changed logging

--- web_honeypot.py
+++ web_honeypot.py
@@ -1,24 +1,27 @@
 import http.server
 import socketserver
-import logging
 import os
 import env_loader
+import logger
 
 PORT = int(env_loader.load("HTTP_PORT"))
 DIRECTORY = env_loader.load("HTTP_DIRECTORY")
+LOG_FILE = env_loader.load("HTTP_LOG")
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
-    
+
     def do_GET(self):
-        logging.info(f"GET request,\nPath: {self.path}\nHeaders:\n{self.headers}")
+        log_data = f"GET request,\nPath: {self.path}\nHeaders:\n{self.headers}"
+        logger.log(log_data + "\n", LOG_FILE)
         super().do_GET()
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        logging.info(f"POST request,\nPath: {self.path}\nHeaders:\n{self.headers}\n\nBody:\n{post_data.decode('utf-8')}")
+        log_data = f"POST request,\nPath: {self.path}\nHeaders:\n{self.headers}\n\nBody:\n{post_data.decode('utf-8')}"
+        logger.log(log_data + "\n", LOG_FILE)
         
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -28,11 +31,10 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
 if not os.path.exists(DIRECTORY):
     os.makedirs(DIRECTORY)
 
-logging.basicConfig(filename='http_server.log', level=logging.INFO)
-logging.info('Starting server...')
+logger.log('Starting server...\n', LOG_FILE)
 
 with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
     print(f"Serving HTTP on port {PORT}")
     print(f"Open http://localhost:{PORT}/ in your browser")
-    logging.info(f"Serving HTTP on port {PORT}")
+    logger.log(f"Serving HTTP on port {PORT}\n", LOG_FILE)
     httpd.serve_forever()
