ca4d3a215267342e0e988f60552dc207ecb6d49f
Shad0wMazt3r <er123pratyaksha@gmail.com>
2024-06-20 21:01:36 +0530

    updated logging

--- logger.py
+++ logger.py
@@ -1,3 +1,8 @@
+import os
+
 def log(log_data, log_file):
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+    log_file = "logs/"+log_file
     with open(log_file, "a") as log:
         log.write(log_data)
