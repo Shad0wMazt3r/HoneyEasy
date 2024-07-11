11c898f660000653b725f34f5a3036c13b99127c
Shad0wMazt3r <er123pratyaksha@gmail.com>
2024-07-12 03:34:05 +0530

    minor changes

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-FILE=".env_var"
+FILE=".installed"
 if [ ! -f "$FILE" ]; then
     sudo apt update
     sudo apt install python3 python-is-python3 python3-venv python3-pip php php-cgi php-mysql php-xml
-    # python3 -m venv honeypot-env
     sudo pip3 install -r requirements.txt
     chmod +x run.sh
-    echo "Installation complete" > .env_var
+    echo "Installation complete" > .installed
 else
     echo "Already installed"
-    bash run.sh
 fi
+bash run.sh
